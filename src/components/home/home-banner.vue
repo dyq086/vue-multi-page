67ee910353b06ad819acf665be56cb2d7924b43a
jiananle <jiananle@gmail.com>
2016-11-03 21:12:16 +0800

    vue+webpack+es6 多页面开发
    vue+webpack+es6 多页面开发

---
+++ src/components/home/home-banner.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home-carousel-container">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-3">{{ msg }}</h1>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'home-banner.vue'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .home-carousel-container {
+        margin: 30px auto;
+    }
+</style>
